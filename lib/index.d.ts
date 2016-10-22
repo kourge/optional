fc9fb4248e6453f09172190547d984327af91044
Wil Lee <kourge@gmail.com>
2016-10-22 00:54:56 -0700

    Commit compiled code

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -27,26 +27,3 @@ export declare function getOrElse<T>(o: Option<T>, defaultValue: Lazy<T>): T;
  * The Lazy<T> type represents either a function that returns T or a plain T.
  */
 export declare type Lazy<A> = A | (() => A);
-/**
- * Wraps an Option<T> in an Optional<T> helper class.
- */
-export declare function toOptional<T>(x: Option<T>): Optional<T>;
-export declare abstract class Optional<A> {
-    abstract toString(): string;
-    abstract orElse(defaultValue: Lazy<A>): A;
-    abstract flatMap<B>(f: (value: A) => Optional<B>): Optional<B>;
-    abstract pureMap<B>(f: (value: A) => B): Optional<B>;
-    abstract map<B>(f: (value: A) => B | Optional<B>): Optional<B>;
-    abstract filter(f: (value: A) => boolean): Optional<A>;
-}
-export declare const None: Optional<any>;
-export declare class Some<A> extends Optional<A> {
-    value: A;
-    constructor(value: A);
-    toString(): string;
-    orElse(defaultValue: Lazy<A>): A;
-    flatMap<B>(f: (value: A) => Optional<B>): Optional<B>;
-    pureMap<B>(f: (value: A) => B): Optional<B>;
-    map<B>(f: (value: A) => B | Optional<B>): Optional<B>;
-    filter(f: (value: A) => boolean): Optional<A>;
-}
